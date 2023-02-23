3affbc30e24f30edb3ab215a38d23892157b4f42
Yelena1212 <Baranchuk1212@gmail.com>
2023-02-22 23:00:56 -0800

    BMI program. type()

---
+++ tip_calculator.py
@@ -0,0 +1,20 @@
+# print("123" + "345")
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print(type(new_num_char))
+# print("Your name has " + new_num_char + " characters")
+
+# a = float(123)
+# print(type(a))
+# # print(70 + float("100.5")
+# print(str(70) + str(100))
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = float(weight) / (float(height) ** 2)
+print(int(bmi))
+
