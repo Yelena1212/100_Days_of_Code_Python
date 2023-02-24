af74a95085d426cbc8e8d9654909f0195a9f5427
Yelena1212 <Baranchuk1212@gmail.com>
2023-02-23 22:02:16 -0800

    Pizza challenge

--- control_flow.py
+++ control_flow.py
@@ -1,18 +1,28 @@
 # print("Welcome to the rollercoaster!")
 # height = int(input("What is your height in cm? "))
-#
+# #
+# bill = 0
 # if height > 120:
 #     print("You can ride the rollercoaster!")
 #     age = int(input("What is your age? "))
 #     if age <= 12:
-#         print("Ticket price is $5")
+#         bill = 5
+#         print("Children ticket price is $5")
 #     elif age <= 18:
-#         print("Ticket price is $7")
+#         bill = 7
+#         print("Youth ticket price is $7")
 #     else:
-#         print("Ticket price is $12")
+#         bill = 12
+#         print("Adult ticket price is $12")
+#
+#     wants_photo = input("Do you want a photo taken? Y or N. ")
+#     if wants_photo == "Y":
+#     # Add $3 to the bill
+#         bill += 3
+#     print(f"You final bill is ${bill}")
 # else:
 #     print("Sorry, you have to grow taller before you can ride.")
-#
+
 
 
 # 🚨 Don't change the code below 👇
@@ -69,12 +79,38 @@
 # 2100 ÷ 400 = 5.25 (Not Leap)
 
 # 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
+# year = int(input("Which year do you want to check? "))
+# # 🚨 Don't change the code above 👆
+#
+# #Write your code below this line 👇
+# if year % 4 == 0 or year % 100 != 0 and year % 400 == 0:
+#     print(f"{year} is Leap year")
+# else:
+#     print(f"{year} is Not Leap year")
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-if year % 4 == 0 or year % 100 != 0 and year % 400 == 0:
-    print(f"{year} is Leap year")
+bill = 0
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
 else:
-    print(f"{year} is Not Leap year")
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
 
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
